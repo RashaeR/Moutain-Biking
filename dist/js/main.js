fc8f6c9a6025ba17ec8157bb686c7e22d5ac08ef
Rashae Redden <rashae_redden@yahoo.com>
2020-01-13 12:00:33 -0500

    Edit Nav bar links

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,6 +1,22 @@
-//Makes Nav unline
+//Variables
+const navLink = document.querySelectorAll(".nav-link");
+
+//Makes Nav unline's length
 for (var i = 0; i < 4; i++) {
-  var nav = document.getElementById("nav-" + String(i));
+  const nav = document.getElementById("nav-" + String(i));
   const getNav = getComputedStyle(nav);
   nav.style.width = String(parseInt(getNav.width.split("px")) + 25) + "px";
+}
+
+underlineEffect(navLink);
+function underlineEffect(link) {
+  for (const eachLink of link) {
+    eachLink.addEventListener("mouseover", function() {
+      eachLink.style.filter = "brightness(120%)";
+    });
+
+    eachLink.addEventListener("mouseleave", function() {
+      eachLink.style.filter = "";
+    });
+  }
 }
